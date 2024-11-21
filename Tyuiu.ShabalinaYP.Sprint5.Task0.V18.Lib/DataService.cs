43a340364ce125eeab2cb1f2431a34766b9d58d2
ShabalinaYulia <julia07.kurash@gmail.com>
2024-11-21 15:18:26 +0500

    Task0

--- Tyuiu.ShabalinaYP.Sprint5.Task0.V18.Lib/DataService.cs
+++ Tyuiu.ShabalinaYP.Sprint5.Task0.V18.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.ShabalinaYP.Sprint5.Task0.V18.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = Path.Combine(Path.GetTempPath(), "OutPutFileTask0.txt"); 
+            string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask0.txt" }); 
             double res = x / (Math.Sqrt(Math.Pow(x, 2) + x));
             res = Math.Round(res, 3);
             File.WriteAllText(path, Convert.ToString(res));
