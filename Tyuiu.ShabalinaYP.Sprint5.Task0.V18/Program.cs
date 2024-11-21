56023b93ea17872ac359aec4a5f30fb226ca3a55
ShabalinaYulia <julia07.kurash@gmail.com>
2024-11-21 14:46:33 +0500

    Task0

--- Tyuiu.ShabalinaYP.Sprint5.Task0.V18/Program.cs
+++ Tyuiu.ShabalinaYP.Sprint5.Task0.V18/Program.cs
@@ -6,17 +6,18 @@ namespace Tyuiu.ShabalinaYP.Sprint5.Task0.V18
         static void Main(string[] args)
         {            
             DataService ds = new DataService();
-            Console.WriteLine("Спринт #4 | Выполнил: Шабалина Ю. П. | ПКТб-24-1");
+            Console.WriteLine("Спринт #5 | Выполнил: Шабалина Ю. П. | ПКТб-24-1");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* Спринт #4                                                               *");
-            Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
-            Console.WriteLine("* Задание #7                                                              *");
-            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #18                                                             *");
             Console.WriteLine("* Выполнил: Шабалина Юлия Павловна | ПКТб-24-1                            *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Дана строка из одноразрядных цифр 983157642891.                         *");
-            Console.WriteLine("* Преобразуйте ее в матрицу 4 на 3 и подсчитайте сумму четных чисел.      *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в  *");
+            Console.WriteLine("* текстовый файл OutPutFileTask0.txt и вывести на консоль.                *");
+            Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
