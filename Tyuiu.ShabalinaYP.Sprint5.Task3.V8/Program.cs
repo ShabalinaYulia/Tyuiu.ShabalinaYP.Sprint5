12dbfc78d01a378c55decd2506be7acf4facda17
ShabalinaYulia <julia07.kurash@gmail.com>
2024-11-23 19:09:27 +0500

    Task3

---
+++ Tyuiu.ShabalinaYP.Sprint5.Task3.V8/Program.cs
@@ -0,0 +1,38 @@
+﻿using Tyuiu.ShabalinaYP.Sprint5.Task3.V8.Lib;
+namespace Tyuiu.ShabalinaYP.Sprint5.Task3.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("Спринт #5 | Выполнил: Шабалина Ю. П. | ПКТб-24-1");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Потоковый метод записи данных в бинарный файл                     *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Шабалина Юлия Павловна | ПКТб-24-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3, результат сохранить в  *");
+            Console.WriteLine("* бинарный файл OutPutFileTask3.bin и вывести на консоль.                 *");
+            Console.WriteLine("* Округлить до трёх знаков после запятой.                                 *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("x^2 + 1");
+            Console.WriteLine("-------");
+            Console.WriteLine("3*x + 4");
+            int x = 3;
+            Console.WriteLine("x = " + 3);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + res);
+            Console.WriteLine("Создан!");
+            Console.ReadKey();
+        }
+    }
+}
